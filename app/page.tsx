7995db2118c60aacd9aa1247d0faf0b1eed4b174
Roman Smirnov <38182485+outjam@users.noreply.github.com>
2025-11-06 14:58:59 +0300

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,47 @@
-export default function Home() {
+// app/page.tsx
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const headersList = await headers();
+  const userAgent = headersList.get("user-agent") || "";
+
+  const isIOS = /iPhone|iPad|iPod/i.test(userAgent);
+  const isAndroid = /Android/i.test(userAgent);
+  const isDesktop = !isIOS && !isAndroid;
+
+  // ✅ iOS → моментальный редирект
+  if (isIOS) {
+    redirect("https://apps.apple.com/app/idXXXXXXX");
+  }
+
+  // ✅ Android → показываем 2 кнопки
+  if (isAndroid) {
+    return (
+      <div>
+        <a href="https://play.google.com/store/apps/details?id=xxx">
+          <button>Google Play</button>
+        </a>
+        <a href="https://apps.rustore.ru/app/xxx">
+          <button>RuStore</button>
+        </a>
+      </div>
+    );
+  }
+
+  // ✅ Desktop → показываем 3 кнопки
   return (
-    <div>
-      Hello
+    <div className='home'>
+
+      <a href="https://apps.apple.com/app/idXXXXXXX">
+        <button>App Store</button>
+      </a>
+      <a href="https://play.google.com/store/apps/details?id=xxx">
+        <button>Google Play</button>
+      </a>
+      <a href="https://apps.rustore.ru/app/xxx">
+        <button>RuStore</button>
+      </a>
     </div>
   );
 }
