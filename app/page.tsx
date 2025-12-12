de8fdc18755e75e80eb02679325662e029e7e2cf
Roman Smirnov <38182485+outjam@users.noreply.github.com>
2025-12-12 11:25:32 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default async function Home() {
 
   return (
     <div className="home">
-      <Snow />
+      {/* <Snow /> */}
 
       <img className="logo" src="/logo.svg" alt="appoint" />
 
